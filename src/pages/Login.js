49b64ca380a0b9fe8baa15bf301d5e7a87b4b60f
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-11-27 13:00:22 -0500

    Basic structure for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Login() {
     return (
-        <div>
-            <h1>Login!</h1>
+        <div className='container container--login'>
+            <div className='row h-100'>
+                <div className='col-md-6 my-auto'>Logo</div>
+                <div className='col-md-6 my-auto'>Input</div>
+            </div>
         </div>
     );
 }
