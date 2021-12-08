f9708afdf9b14241aa4ec319fc2bfee05f9623b7
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-12-08 18:35:09 -0500

    Added userName handling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,19 @@ import { useAuth } from "../contexts/AuthContext";
 export default function Home() {
     const { currentUser } = useAuth();
 
+    let name;
+
+    if (currentUser) {
+        name = currentUser.displayName
+            ? currentUser.displayName.split(" ")[0]
+            : currentUser.name
+            ? currentUser.name
+            : null;
+    }
+
     return currentUser ? (
         <div className='home'>
-            <h1>Hi, {currentUser.displayName}</h1>!
+            <h1>Hi{name ? `, ${name}` : ""}!</h1>
             <h2>Please select your available streaming services</h2>
             <ServiceSelect />
         </div>
