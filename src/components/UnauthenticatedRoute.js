5511ae0107b3141bb26286dccadd2ffefa99a393
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-12-08 18:29:31 -0500

    Removed cloneElement and replaced with outlet

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -1,12 +1,10 @@
-import React, { cloneElement } from "react";
-import { Route, Navigate } from "react-router-dom";
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function UnauthenticatedRoute(props) {
     const { children, ...rest } = props;
     const { isLoggedIn } = useAuth();
 
-    return (
-        <Route {...rest}>{!isLoggedIn ? cloneElement(children, props) : <Navigate to='/' />}</Route>
-    );
+    return !isLoggedIn ? <Outlet /> : <Navigate to='/' />;
 }
